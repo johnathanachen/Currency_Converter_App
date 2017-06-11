dc87393c089608c9c983c56dedabef1a4d1f3f36
jc.swift <jc.swift@icloud.com>
2017-06-11 03:36:38 -0500

    build out home screen with logo animations, button, and input textfield

---
+++ app/components/Text/styles.js
@@ -0,0 +1,11 @@
+import EStylesSheet from 'react-native-extended-stylesheet';
+
+const styles = EStylesSheet.create({
+  smallText: {
+    color: '$white',
+    fontSize: 12,
+    textAlign: 'center'
+  }
+});
+
+export default styles;
